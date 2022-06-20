f77088eac11e1936f606aeffd9d72daa33a30c86
Shivashish Das <dasshivashish11@gmail.com>
2022-06-21 02:35:44 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CommandLine;
+
+namespace Lang {
+	public class Options {
+		[Option ('f', "file", Required = true, HelpText = "Specifies the input file")]
+		public string? file {get; set;} 
+	}
+
+	internal class Start {
+		static void Main(string[] args) {
+			Parser.Default.ParseArguments<Options>(args)
+				.WithParsed(CheckOpts);
+		}
+
+		private static void CheckOpts(Options o) {
+		}
+	}
+}
