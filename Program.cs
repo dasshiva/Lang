050fb599ccee4e7461389bb4cda42b403adcc62d
Shivashish <dasshivashish11@gmail.com>
2022-06-21 14:00:59 +0530

    strated working on lexer

--- Program.cs
+++ Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
+using System.IO;
 using CommandLine;
 
 namespace Lang {
 	public class Options {
 		[Option ('f', "file", Required = true, HelpText = "Specifies the input file")]
-		public string? file {get; set;} 
+		public string File {get; set;} 
 	}
 
 	internal class Start {
+		static StringReader? fs = null;
 		static void Main(string[] args) {
 			Parser.Default.ParseArguments<Options>(args)
 				.WithParsed(CheckOpts);
+			
 		}
 
 		private static void CheckOpts(Options o) {
+			try {
+				fs = new StringReader(o.File);
+			} catch (Exception e) {
+				Console.WriteLine(e.Message);
+				Environment.Exit(-1);
+			}
 		}
 	}
 }
